6d9bb7dec33f7740d107183716af443086f9e5e0
AdityaDabgotra <adityadabgotra2004@gmail.com>
2026-01-18 20:28:57 +0530

    data transformation completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Text_Summarizer.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from Text_Summarizer.logging import logger
 from Text_Summarizer.pipeline.stage02_data_validation import DataValidationTrainingPipeline
+from Text_Summarizer.pipeline.stage03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -20,6 +21,18 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_validation = DataValidationTrainingPipeline()
    data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
